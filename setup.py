f1a8f8507c1e50340dee81a1d2da23ea36101e81
Ren Quinn <renquinn@gmail.com>
2021-01-04 12:27:37 -0700

    Forgot to include in the install requirements

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    install_requires = fh.read().splitlines()
 
 setuptools.setup(
     name="easel-cli",
-    version="0.0.1",
+    version="0.0.2",
     author="Ren Quinn",
     author_email="renquinn@gmail.com",
     description="A Canvas course management tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/renquinn/easel-py",
+    install_requires=install_requires,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
