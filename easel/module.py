a698816f3bc8994155f2b506b427be4466a94a97
Ren Quinn <renquinn@gmail.com>
2021-07-16 12:00:14 -0600

    Pull modules (and improve pushing as well)

--- easel/module.py
+++ easel/module.py
@@ -1,10 +1,19 @@
+import logging
+import os.path
+import urllib.parse
+
+from easel import assignment
 from easel import canvas_id
 from easel import component
 from easel import course
+from easel import files
+from easel import helpers
+from easel import page
 
 MODULES_PATH=course.COURSE_PATH+"/modules"
 MODULE_PATH=MODULES_PATH+"/{}"
 MODULES_TABLE="modules"
+MODULES_DIR="modules"
 WRAPPER="module"
 
 class Module(component.Component):
@@ -21,11 +30,21 @@ class Module(component.Component):
         self.unlock_at = unlock_at
         self.require_sequential_progress = require_sequential_progress
         self.prerequisite_module_ids = prerequisite_module_ids
-        # Items can only be added after the fact
+        # Items can only be added after the fact (unless pulling)
         self.items = items
 
     def __repr__(self):
         return f"Module(name={self.name}, position={self.position})"
+
+    @classmethod
+    def build(cls, fields):
+        extras = ["id", "workflow_state", "items_count", "items_url", "state",
+                "completed_at", "publish_final_grade"]
+        defaults = [("require_sequential_progress", False),
+                ("prerequisite_module_ids", []),
+                ("unlock_at", "")]
+        component.filter_fields(fields, extras, defaults)
+        return Module(**fields)
 
     def postprocess(self, db, course_, dry_run):
         course_id = course_.canvas_id
@@ -37,14 +56,44 @@ class Module(component.Component):
             print("make sure the module has first been pushed to Canvas")
             return
 
-        for item in self.items:
+        for i in range(len(self.items)):
+            item = self.items[i]
             if isinstance(item, dict):
-                item["filename"] = f"{self.filename}--{item['item']}"
+                # yaml objects (dictionaries) should be properly formatted
+                if self.filename and 'item' in item:
+                    item["filename"] = f"{self.filename}--{item['item']}"
+                elif'external_url' in item:
+                    # URL
+                    item["filename"] = f"{self.filename}--{item['external_url']}"
+                    if 'published' not in item:
+                        item['published'] = True
+                elif 'title' in item:
+                    # SubHeader
+                    item["filename"] = f"{self.filename}--{item['title']}"
+                    if 'published' not in item:
+                        item['published'] = True
+                item["position"] = i+1
                 item_component = component.build("ModuleItem", item)
             elif isinstance(item, str):
-                item_component = component.build("ModuleItem", {
-                    "item": item,
-                    "filename": f"{self.filename}--{item}"})
+                # possible string-only options: filename, url, SubHeader
+                base_item = {
+                        "filename": f"{self.filename}--{item}",
+                        "position": i+1
+                        }
+                if os.path.isfile(item):
+                    base_item['item'] = item
+                elif helpers.isurl(item):
+                    base_item["external_url"] = item
+                    # if title is blank, canvas says "No Title" on the module
+                    # item, displaying the url instead seems like a better
+                    # default
+                    base_item["title"] = item
+                    base_item["published"] = True
+                else:
+                    # assume SubHeader
+                    base_item["title"] = item
+                    base_item["published"] = True
+                item_component = component.build("ModuleItem", base_item)
             else:
                 raise TypeError(f"Invalid item specification on module {self}")
 
@@ -54,3 +103,98 @@ class Module(component.Component):
 # Needed for custom yaml tag
 def constructor(loader, node):
     return Module(**loader.construct_mapping(node))
+
+def build_item(item, indent=0):
+    if indent == 0:
+        return item
+    return {'item': item, 'indent': indent}
+
+def pull_all(db, course_, dry_run):
+    modules = []
+    m = helpers.get(MODULES_PATH.format(course_.canvas_id),
+            params={'include': ['items']}, dry_run=dry_run)
+    for module_ in m:
+        if 'items' not in module_ or not module_['items']:
+            items_path = urllib.parse.urlparse(module_['items_url']).path
+            module_['items'] = helpers.get(items_path, dry_run=dry_run)
+        cid = canvas_id.find_by_id(db, course_.canvas_id, module_.get('id'))
+        if cid:
+            module_['filename'] = cid.filename
+        else:
+            module_['filename'] = component.gen_filename(MODULES_DIR,
+                    str(module_.get('position')) + '-' + module_.get('name',''))
+            cid = canvas_id.CanvasID(module_['filename'], course_.canvas_id)
+            cid.canvas_id = module_.get('id')
+            cid.save(db)
+
+        items = {}
+        for item in module_['items']:
+            item_id = 0
+            if item['type'] == "Page":
+                item_id = item['page_url']
+            elif item['type'] in ['File', 'Assignment', 'Quiz']:
+                item_id = item['content_id']
+            # SubHeaders and ExternalUrls won't be in the db anyway
+            icid = canvas_id.find_by_id(db, course_.canvas_id, item_id)
+            if item_id == 0 or not icid:
+                # we don't have the item locally so try to pull it
+                if item['type'] == "Page":
+                    _, icid = page.pull_page(db, course_.canvas_id,
+                            item['page_url'], dry_run)
+                elif item['type'] == "File":
+                    # get file info
+                    file_path = urllib.parse.urlparse(item['url']).path
+                    file_ = helpers.get(file_path, dry_run=dry_run)
+                    url = file_['url']
+                    # get parent folder info
+                    folder_path = files.COURSE_FOLDERS_PATH.format(course_.canvas_id)+"/"+str(file_['folder_id'])
+                    folder = helpers.get(folder_path, dry_run=dry_run)
+                    path = folder['full_name'][len("course "):]
+                    filepath = path + file_['filename']
+                    # download the file
+                    icid = files.pull_file(db, course_.canvas_id, url, file_['id'], filepath)
+                elif item['type'] == 'Assignment':
+                    _, icid = assignment.pull(db, course_.canvas_id, item_id, dry_run)
+                elif item['type'] == 'Quiz':
+                    # TODO: pull quiz
+                    logging.warn("Cannot pull quizzes yet...")
+                    continue
+                elif item['type'] == 'SubHeader':
+                    built_item = {'title': item['title']}
+                    if not item['published']:
+                        built_item['published'] = False
+                elif item['type'] == 'ExternalUrl':
+                    built_item = {
+                        'title': item.get('title', ''),
+                        'external_url': item['external_url']}
+                    if not item['published']:
+                        built_item['published'] = False
+                else:
+                    logging.warn("I can't find the module item "
+                            f"'{item['title']}' locally and I couldn't figure "
+                            "out how to pull it because it is not an "
+                            "easel-supported type: " + item['type'])
+                    continue
+
+            position = item.get('position', 0)
+            if position not in items:
+                items[position] = []
+            if icid:
+                built_item = {"item": icid.filename, "indent": item.get('indent', 0)}
+                if item['type'] == 'File' and 'title' in item:
+                    built_item['title'] = item['title']
+            if 'indent' in built_item and built_item['indent'] == 0:
+                del built_item['indent']
+            items[position].append(built_item)
+
+        # order items by position
+        all_items = []
+        for position in sorted(items.keys()):
+            if position == 0:
+                # we used 0 as default above so skip that for now
+                continue
+            all_items += items[position]
+        # add positionless items to the end
+        module_['items'] = all_items + items.get(0, [])
+        modules.append(Module.build(module_))
+    return modules
--- easel/module_item.py
+++ easel/module_item.py
@@ -1,5 +1,9 @@
+import logging
+
 from easel import component
 from easel import canvas_id
+from easel import files
+from easel import helpers
 from easel import helpers_yaml
 from easel import module
 
@@ -12,9 +16,9 @@ VALID_TYPES = ["File", "Page", "Discussion", "Assignment", "Quiz", "SubHeader",
 
 class ModuleItem(component.Component):
 
-    def __init__(self, item="", title=None, type=None, content_id=None,
+    def __init__(self, item=None, title=None, type=None, content_id=None,
             position=None, indent=None, page_url=None, external_url=None,
-            new_tab=None, filename=""):
+            published=None, new_tab=None, filename=""):
         super().__init__(create_path=MODULE_ITEMS_PATH, update_path=MODULE_ITEM_PATH,
                 db_table=MODULE_ITEMS_TABLE, canvas_wrapper=WRAPPER,
                 filename=filename)
@@ -27,6 +31,7 @@ class ModuleItem(component.Component):
         self.page_url = page_url
         self.external_url = external_url
         self.new_tab = new_tab
+        self.published = published
 
     def __repr__(self):
         return f"ModuleItem(item={self.item}, type={self.type}, title={self.title})"
@@ -45,9 +50,40 @@ class ModuleItem(component.Component):
         cid.find_id(db)
         return self.update_path.format(course_id, cid.canvas_id, path_args[1])
 
-    def preprocess(self, db, course_id, dry_run):
+    def load_module(self, db, course_id):
+        if self.filename:
+            module_filename = self.filename.split('--')[0]
+            m = helpers_yaml.read(module_filename)
+            m.filename = module_filename
+            return m
+
+    def preprocess(self, db, course_, dry_run):
+        if not self.item:
+            # it should be a url or SubHeader so we only have to set the type
+            # and filename
+            if self.external_url:
+                self.type = "ExternalUrl"
+            else:
+                self.type = "SubHeader"
+            return
+        cid = canvas_id.CanvasID(self.item, course_.canvas_id)
+        cid.find_id(db)
+        if self.item.startswith("files"):
+            # TODO: need a better way to detect that this is a literal File to
+            # link to and not just a yaml file containing item info such as for
+            # a Page or Assignment
+            self.type = "File"
+            if not cid.canvas_id:
+                # the file probably doesn't exist in the course so we need to
+                # push it first
+                files.push(db, course_, self.item, True, dry_run)
+            return
         item = helpers_yaml.read(self.item)
         item.filename = self.item
+        if not cid.canvas_id:
+            # the component probably doesn't exist in the course so we need to
+            # push it first
+            item.push(db, course_, dry_run)
         if not self.title:
             self.title = getattr(item, "name", getattr(item, "title", self.item))
         type_ = type(item).__name__
@@ -55,8 +91,32 @@ class ModuleItem(component.Component):
             raise ValueError(f"Cannot add an item of type {type_} to a module."
                     " Can be one of {VALID_TYPES}")
         self.type = type_
-        cid = item.get_canvas_id(db, course_id)
+        cid = item.get_canvas_id(db, course_.canvas_id)
         if self.type == "Page":
             self.page_url = cid
         else:
             self.content_id = cid
+
+    def postprocess(self, db, course_, dry_run):
+        course_id = course_.canvas_id
+        if self.type not in ["ExternalUrl", "SubHeader"]:
+            return
+        cid = canvas_id.CanvasID(self.filename, course_id)
+        cid.find_id(db)
+        if not cid.canvas_id:
+            print(f"Failed to publish {self}, we don't "
+                    "have a canvas id for it")
+            print("Make sure the module item was succesfully pushed to Canvas")
+            return
+        # since Canvas doesn't allow you to publish some ModuleItems
+        # (ExternalUrl, SubHeader) on creation, we'll update the same item here
+        # to make sure it gets published (if desired; when 'published' is False
+        # it won't be published)
+        parent_module = self.load_module(db, course_id)
+        path = self.format_update_path(db, course_id, cid.canvas_id, parent_module)
+        self.preprocess(db, course_id, dry_run)
+        resp = helpers.put(path, self, dry_run=dry_run)
+        if "errors" in resp:
+            print(f"failed to publish {self}")
+            for error in resp['errors']:
+                logging.error(error['message'])
